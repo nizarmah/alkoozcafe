876cfc3d625d7d40e2e8b3134d8441150c50ab19
Nizar <nizarmah@hotmail.com>
2023-01-12 02:35:54 +0200

    Prevent certain actions on menu singleton

--- backend/deskStructure.ts
+++ backend/deskStructure.ts
@@ -1,13 +1,25 @@
+import { DocumentActionsResolver } from 'sanity'
 import { StructureBuilder, StructureResolver } from 'sanity/desk'
 
-export const structure: StructureResolver = (S: StructureBuilder) =>
+export const deskAction: DocumentActionsResolver = (prev, context) => {
+  if (context.schemaType === 'menu') {
+    return prev.filter(({ action }) =>
+      action === 'publish'
+    )
+  }
+
+  return prev
+}
+
+export const deskStructure: StructureResolver = (S: StructureBuilder) =>
   S.list()
-  .title('Base')
+  .title('Content')
   .items([
     S.listItem()
       .title('Menu')
       .child(
-        S.document()
+        S.editor()
+          .id('menu')
           .schemaType('menu')
           .documentId('menu')),
           ...S.documentTypeListItems().filter(listItem => { 
