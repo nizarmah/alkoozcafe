f781d1682e9a7711d516b94937febad1ab0292f6
Nizar <nizarmah@hotmail.com>
2023-01-26 21:36:28 +0200

    Update name to include Restaurant

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -15,7 +15,7 @@ const Header: React.FC<HeaderProps> = ({ className }) => {
             <div className='absolute inset-x-0 bottom-0 h-px bg-slate-900/10'></div>
             <a className='relative mr-auto flex grow items-center ' href='/'>
               <Logo className='mx-4 h-8 print:h-6 top-[20%] fill-slate-900' />
-              <span className='text-2xl print:text-2xl block flex-1'>Al Kooz Café</span>
+              <span className='text-2xl print:text-2xl block flex-1'>Al Kooz Café & Restaurant</span>
             </a>
           </div>
         </nav>
