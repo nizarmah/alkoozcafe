409f4e86b589f21c65f6c5eb1c34032cfaaf80ab
Nizar <nizarmah@hotmail.com>
2023-04-22 20:11:19 +0300

    Improve code formatting

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -15,7 +15,11 @@ const IndexPage: React.FC<IndexPageProps> = ({}) => {
     <main className='text-slate-900'>
       <Seo />
 
-      <iframe className='w-screen h-screen' frameBorder={0} src={`https://docs.google.com/viewer?embedded=true&url=${pdfMenuUrl}`}>
+      <iframe
+        className='w-screen h-screen'
+        frameBorder={0}
+        src={`https://docs.google.com/viewer?embedded=true&url=${pdfMenuUrl}`}
+      >
         <Background />
         <div className='px-2 lg:px-24 lg:px-16 md:px-8'>
           <Header className='w-full' />
@@ -23,7 +27,12 @@ const IndexPage: React.FC<IndexPageProps> = ({}) => {
             <div className='pt-12 pb-4 px-6 md:pt-28 md:pb-12'>
               <h1 className='text-4xl font-bold mb-16'>Something went wrong</h1>
               <p>Looks like your browser cannot display the Menu</p>
-              <Link to='/Al%20Kooz%20Cafe%20—%20Menu.pdf' className='text-md font-normal'>Click here to download the Menu</Link>
+              <Link
+                to='/Al%20Kooz%20Cafe%20—%20Menu.pdf'
+                className='text-md font-normal'
+              >
+                Click here to download the Menu
+              </Link>
             </div>
           </div>
         </div>
