642a4d0d262347f0e7af317b24462b5f30b85c0a
Nizar <nizarmah@hotmail.com>
2023-04-22 16:00:49 +0300

    Improve unsupported error message

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
-import type { HeadFC } from 'gatsby'
+import { HeadFC, Link } from 'gatsby'
 
 import Seo from '../components/Seo'
+import Background from '../components/Background'
+import Header from '../components/Header'
 
 type IndexPageProps = {}
 
@@ -10,8 +12,18 @@ const IndexPage: React.FC<IndexPageProps> = ({}) =>
     <main className='text-slate-900'>
       <Seo />
 
-      <object className='w-screen h-screen' data="/Al%20Kooz%20Cafe%20—%20Menu.pdf" type="application/pdf">
-        <p>Looks like your browser can't display the menu — <a href="/Al%20Kooz%20Cafe%20—%20Menu.pdf">Click here to download it.</a></p>
+      <object className='w-screen h-screen' data="/static/Al%20Kooz%20Cafe%20—%20Menu.pdf" type="application/pdf">
+        <Background />
+        <div className='px-2 lg:px-24 lg:px-16 md:px-8'>
+          <Header className='w-full' />
+          <div className='w-full xl:w-2/3 xl:px-12 lg:w-5/6 lg:px-6'>
+            <div className='pt-12 pb-4 px-6 md:pt-28 md:pb-12'>
+              <h1 className='text-4xl font-bold mb-16'>Something went wrong</h1>
+              <p>Looks like your browser cannot display the Menu</p>
+              <Link to='/Al%20Kooz%20Cafe%20—%20Menu.pdf' className='text-md font-normal'>Click here to download the Menu</Link>
+            </div>
+          </div>
+        </div>
       </object>
     </main>
   )
